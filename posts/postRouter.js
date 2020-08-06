8028c85eb1480275f52c4b511be58bdfb9e632f6
Eorium <travisgent312@gmail.com>
2020-08-06 17:23:09 -0600

    add finished project

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const db = require("./postDb.js");
+const postDataBase = require("./postDb.js");
+const userDataBase = require("../users/userDb.js");
 
 router.get('/', (req, res) => {
-  db.get()
+  postDataBase.get()
     .then(posts => {
       res.status(200).json({ data: posts})
     })
@@ -15,22 +16,59 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  const { id } = req.params;
+  postDataBase.getById(id)
+    .then(posts => {
+      res.status(200).json({ data: posts})
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "We could not get the data" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+  const { id } = req.params;
+  postDataBase.remove(id)
+    .then(post => {
+      res.status(201).json({ data: post });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not delete the post" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  const { id } = req.params;
+  const newPost = req.body;
+
+  db.update(id, newPost)
+    .then(post => {
+      res.status(201).json({ data: post });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not edit the post" });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  userDataBase.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        next();
+      } else {
+        res.status(404).json({ message: "post not found" })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not find the post" });
+    })
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,18 +2,38 @@ const express = require('express');
 
 const router = express.Router();
 
-const db = require("./userDb.js");
+const postDataBase = require("../posts/postDb.js");
+const userDataBase = require("./userDb.js");
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+  const newUser = req.body;
+  userDataBase.insert(newUser)
+    .then(post => {
+      res.status(201).json({ data: post });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "We could not add the post" });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validatePost, (req, res) => {
+  const { id } = req.params;
+  const newPost = req.body;
+  newPost.user_id = id;
+
+  postDataBase.insert(newPost)
+    .then(post => {
+      res.status(201).json({ data: post });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "We could not add the post" });
+    });
 });
 
 router.get('/', (req, res) => {
-  db.get()
+  userDataBase.get()
     .then(users => {
       res.status(200).json({ data: users})
     })
@@ -23,34 +43,102 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
+  userDataBase.getById(id)
+    .then(users => {
+      res.status(200).json({ data: users})
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "We could not get the data" });
+    });
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+  postDataBase.get()
+    .then(posts => {
+      const userPosts = posts.filter(newPost => parseInt(newPost.user_id) === parseInt(id));
+      res.status(200).json({ data: userPosts })
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "We could not get the data" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
+  userDataBase.remove(id)
+    .then(user => {
+      res.status(201).json({ data: user });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not delete the user" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
+  const newUser = req.body;
+
+  userDataBase.update(id, newUser)
+    .then(user => {
+      res.status(201).json({ data: user });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not edit the user" });
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
-}
+  userDataBase.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        next();
+      } else {
+        res.status(404).json({ message: "user not found" })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not find the user" });
+    })
+};
 
 function validateUser(req, res, next) {
-  // do your magic!
+  userDataBase.insert(req.body)
+    .then(user => {
+      if (user) {
+        next();
+      } else {
+        res.status(404).json({ message: "missing data" })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not find the data" });
+    })
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  postDataBase.insert(req.body)
+    .then(user => {
+      if (user) {
+        next();
+      } else {
+        res.status(404).json({ message: "missing data" })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "We could not find the data" });
+    })
 }
 
 module.exports = router;
